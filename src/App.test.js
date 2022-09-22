4789342351080898867bc024fac038cf820b518b
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-09-22 15:53:04 +0200

    check branch rule

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react linkkk", () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/React test project/i);
   expect(linkElement).toBeInTheDocument();
