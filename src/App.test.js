8177cabfd10ced7a14b1598d06e88b57794358c9
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-09-22 15:43:01 +0200

    test protection rule

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders learn react linkk", () => {
   render(<App />);
   const linkElement = screen.getByText(/React test project/i);
   expect(linkElement).toBeInTheDocument();
