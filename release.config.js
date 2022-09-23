dc31479b1f415f2ba456157d3e15124aa3fa834d
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-09-23 16:07:56 +0200

    feat: fix format issue

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/Northern-Data-AG/test_react_app",
-    plugins: ['@semantic-release/commit-analyzer',
-             '@semantic-release/release-notes-generator',
-             '@semantic-release/github']
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/Northern-Data-AG/test_react_app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
