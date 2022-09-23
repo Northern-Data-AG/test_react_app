ecd6c6c18165a09f6b1a7eda2f75c96fe166dd7a
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-09-23 16:43:35 +0200

    feat: change branch name

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: "main",
   repositoryUrl: "https://github.com/Northern-Data-AG/test_react_app",
   plugins: [
     "@semantic-release/commit-analyzer",
