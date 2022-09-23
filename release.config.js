e84fd86bff35d72fbf31b022c9bc3af713a0a2e6
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-09-23 15:52:33 +0200

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/Northern-Data-AG/test_react_app",
+    plugins: ['@semantic-release/commit-analyzer',
+             '@semantic-release/release-notes-generator',
+             '@semantic-release/github']
+}
